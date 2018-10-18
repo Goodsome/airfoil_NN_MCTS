b0cc38751f8ca7181cab4e07aa291272bd2c2d68
谢李兴 <xie@xxx.local>
2018-10-18 13:39:16 +0800

    n=101

--- input_data.py
+++ input_data.py
@@ -1,20 +1,24 @@
-from mcts import point_index
+from mcts import point_index, index_point
 import numpy as np
 
 
-def n_11():
-    n = 11
-    airfoil = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                        0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-                        0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-                        0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-                        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-                        0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-                        0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-                        0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).reshape([n, n])
+e = 0.000001
+
+airfoil = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+                    0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+                    0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+                    0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+                    0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).reshape([11, 11])
+
+def inputs_data(airfoil):
+
+    n = airfoil.shape[0]
 
     c_n2 = n * (n - 1) // 2
     air_input = np.zeros([n - 2, n, n])
@@ -29,4 +33,28 @@ def n_11():
 
 
 def naca0012():
-    tmp = np.loadtxt('dian0.csv', dtype=np.str, delimiter=',')
+    # tmp = np.loadtxt('dian0.csv', dtype=np.str, delimiter=',')
+    x = np.linspace(0, 1, 101)
+    z = 0.1
+    points = np.zeros([200, 2])
+    points[:101, 0] = x[::-1]
+    points[101:, 0] = x[1:-1]
+    points[1:100, 1] = -z
+    points[101:, 1] = z
+
+    points *= 100
+    points[:, 1] += 50
+    points += e
+
+    air = np.zeros([101, 101])
+    for i in points:
+        air[i.astype(np.int)[1], i.astype(np.int)[0]] = 1
+    return air
+
+if __name__ == '__main__':
+    airfoil_shape = naca0012()
+
+    np.set_printoptions(threshold = np.nan)
+    x_input, y_true = inputs_data(airfoil_shape)
+    print(x_input[4])
+    print(np.argwhere(y_true == 1))
